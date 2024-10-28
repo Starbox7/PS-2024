169ae1464a9ab92c948671518d2a2aa89e396d47
Starbox <39715563+Starbox7@users.noreply.github.com>
2024-10-28 09:48:12 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.53 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,34 +1,17 @@
 function solution(today, terms, privacies) {
-    var answer = [];
-    
-    const todayArray = today.split('.').map(Number)
-    let stateArray = []
-    let termsJson = {}
-    
-    terms.forEach((term) => {
-        const [type, duration] = term.split(' ');
-        termsJson[type] = Number(duration);
-    })
-    
-    privacies.forEach((privacy, index) => {
-        const [date, type] = privacy.split(' ');
-        const privacyDateArray = date.split('.').map(Number);
-        
-        privacyDateArray[1] += termsJson[type];
-        if (privacyDateArray[1] > 12) {
-            privacyDateArray[0] += Math.floor((privacyDateArray[1] - 1) / 12);
-            privacyDateArray[1] = (privacyDateArray[1] - 1) % 12 + 1;
-        }
-        
-        if (
-            privacyDateArray[0] < todayArray[0] ||
-            (privacyDateArray[0] === todayArray[0] && privacyDateArray[1] < todayArray[1]) ||
-            (privacyDateArray[0] === todayArray[0] && privacyDateArray[1] === todayArray[1] && privacyDateArray[2] <= todayArray[2])
-        ) {
-            answer.push(index + 1);
-        }
-    })
-    
-    
-    return answer;
+  var answer = [];
+  var [year, month, date] = today.split(".").map(Number);
+  var todates = year * 12 * 28 + month * 28 + date;
+  var t = {};
+  terms.forEach((e) => {
+    let [a, b] = e.split(" ");
+    t[a] = Number(b);
+  });
+  privacies.forEach((e, i) => {
+    var [day, term] = e.split(" ");
+    day = day.split(".").map(Number);
+    var dates = day[0] * 12 * 28 + day[1] * 28 + day[2] + t[term] * 28;
+    if (dates <= todates) answer.push(i + 1);
+  });
+  return answer;
 }
