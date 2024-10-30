0585eb0e35c6e2ba56a2b5f0e935f108bcaa71ed
Starbox <39715563+Starbox7@users.noreply.github.com>
2024-10-31 08:48:33 +0900

    [level 2] Title: 귤 고르기, Time: 24.44 ms, Memory: 47.2 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -2,16 +2,13 @@ function solution(k, tangerine) {
     var answer = 0;
     var temp = 0;
 
-    // Map을 사용하여 각 귤의 크기별 빈도수 카운팅
     const countMap = new Map();
     tangerine.forEach((t) => {
         countMap.set(t, (countMap.get(t) || 0) + 1);
     });
 
-    // 빈도수를 내림차순으로 정렬
     const sortedCounts = Array.from(countMap.values()).sort((a, b) => b - a);
 
-    // 정렬된 빈도수 배열에서 필요한 만큼 누적하여 `k` 이상이 되도록 함
     for (let i = 0; i < sortedCounts.length; i++) {
         temp += sortedCounts[i];
         if (temp >= k) {
